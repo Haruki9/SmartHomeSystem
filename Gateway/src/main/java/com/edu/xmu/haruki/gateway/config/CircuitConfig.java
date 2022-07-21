f5edc2aecc85ff2874268fb7610b54f6b96efd0b
Haruki9 <8832452+haruki9@user.noreply.gitee.com>
2022-07-21 13:30:29 +0800

    delete class files.

--- Gateway/src/main/java/com/edu/xmu/haruki/gateway/config/CircuitConfig.java
+++ Gateway/src/main/java/com/edu/xmu/haruki/gateway/config/CircuitConfig.java
@@ -4,7 +4,6 @@ import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
